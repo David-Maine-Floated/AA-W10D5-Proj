c069b5ed0e132e1bb113c06941d218b535550126
David <dmaine6711@gmail.com>
2023-12-21 14:48:04 -0500

    finish phase 4

--- aa14-react-router-art-museum-full-time/art-museum/src/App.jsx
+++ aa14-react-router-art-museum-full-time/art-museum/src/App.jsx
@@ -1,24 +1,50 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import harvardArt from './data/harvardArt';
-
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Outlet,
+    NavLink
+} from "react-router-dom";
+import harvardArt from "./data/harvardArt";
+import GalleryNavigation from "./components/GalleryNavigation/GalleryNavigation";
 console.log(harvardArt);
 
 const router = createBrowserRouter([
-  {
-    path: '*',
-    element: <h2>Page Not Found</h2>,
-  },
-  {
-    path: '/',
-    element: <>
-      <h2>Harvard Art Museum</h2>
-      <p>Look, but Don&apos;t Touch. Please select a Gallery in the navigation bar.</p>
-    </>,
-  },
+    {
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: (
+                    <>
+                        <h2>Harvard Art Museum</h2>
+                        <p>
+              Look, but Don&apos;t Touch. Please select a Gallery in the
+              navigation bar.
+                        </p>
+                    </>
+                ),
+            },
+        ],
+    },
+    {
+        path: "*",
+        element: <h2>Page Not Found</h2>,
+    },
 ]);
 
+function Layout() {
+    return (
+        <div className="page-wrapper">
+            <GalleryNavigation galleries={harvardArt.records} />
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+}
+
 function App() {
-  return <RouterProvider router ={router} />;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ aa14-react-router-art-museum-full-time/art-museum/src/components/GalleryNavigation/GalleryNavigation.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from "react-router-dom";
+import "./GalleryNavigation.css";
+
+export default function GalleryNavigation({ galleries }) {
+  console.log("galleries:", galleries);
+  return (
+    <nav>
+      <h1>Galleries</h1>
+      <NavLink to="/">Home</NavLink>
+      <br></br>
+
+      {galleries.map((gallery) => (
+        <NavLink key={gallery.id} to={`gallery/${gallery.id}`}>
+          {gallery.name} <br></br>
+        </NavLink>
+      ))}
+    </nav>
+  );
+}
