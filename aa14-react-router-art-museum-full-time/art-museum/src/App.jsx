57dc4d2a880c1decfebf7009cb1e5313255e05ef
Ishan Chawla <ishan.kpv@gmail.com>
2023-12-21 14:03:23 -0500

    ph1

--- aa14-react-router-art-museum-full-time/art-museum/src/App.jsx
+++ aa14-react-router-art-museum-full-time/art-museum/src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import harvardArt from './data/harvardArt'
+import harvardArt from './data/harvardArt';
 
 console.log(harvardArt);
 
@@ -7,6 +7,13 @@ const router = createBrowserRouter([
   {
     path: '*',
     element: <h2>Page Not Found</h2>,
+  },
+  {
+    path: '/',
+    element: <>
+      <h2>Harvard Art Museum</h2>
+      <p>Look, but Don&apos;t Touch. Please select a Gallery in the navigation bar.</p>
+    </>,
   },
 ]);
 
